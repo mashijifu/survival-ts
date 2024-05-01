8f268eaf8412eba5d681270fbe84e8f87736ec90
mashijifu <4ts0t0i7c@gmail.com>
2024-05-01 21:39:21 +0900

    Jestを使用したスナップショットテストのやり方と作り方

--- component-test-tutorial/src/SimpleButton.test.tsx
+++ component-test-tutorial/src/SimpleButton.test.tsx
@@ -1,12 +1,22 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+// import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+// import { SimpleButton } from "./SimpleButton";
+
+// teting-libraryを使用したテスト
+// test("ボタンをクリックするとON/OFFの表示が切り替わる", async () => {
+//   const user = userEvent.setup();
+//   render(<SimpleButton />);
+//   const simpleButton = screen.getByRole("button");
+//   expect(simpleButton).toHaveTextContent("OFF");
+//   await user.click(simpleButton);
+//   expect(simpleButton).toHaveTextContent("ON");
+// });
+
+import { render } from "@testing-library/react";
 import { SimpleButton } from "./SimpleButton";
- 
-test("ボタンをクリックするとON/OFFの表示が切り替わる", async () => {
-  const user = userEvent.setup();
-  render(<SimpleButton />);
-  const simpleButton = screen.getByRole("button");
-  expect(simpleButton).toHaveTextContent("OFF");
-  await user.click(simpleButton);
-  expect(simpleButton).toHaveTextContent("ON");
+
+// Jestを使用したスナップショットテスト
+test("描画されてすぐはOFFと表示されている", () => {
+  const view = render(<SimpleButton />);
+  expect(view.container).toMatchSnapshot();
 });
